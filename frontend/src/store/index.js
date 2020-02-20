a4bc2fc2dd974c5096c8311fbce6073c50f89963
Felipe Lima <felipinho.lima27@gmail.com>
2020-02-20 00:14:54 -0300

    Cadastro e autenticação de usuários

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,19 +1,24 @@
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+
 import createStore from './createStore';
+import persistReducers from './persistReducers';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMonitor = process.env.NODE_ENV === 'development'
-  ? console.tron.createSagaMonitor()
-  : null;
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
 
 const SagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middleware = [SagaMiddleware];
 
-const store = createStore(rootReducer, middleware);
+const store = createStore(persistReducers(rootReducer), middleware);
+const persistor = persistStore(store);
 
 SagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };

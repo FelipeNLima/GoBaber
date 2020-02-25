f94023b2a378362d269f64282e498272dd3c90be
Felipe Lima <felipinho.lima27@gmail.com>
2020-02-25 16:06:47 -0300

    Configurando o header e notificações

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
   return (
-    <div />
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" placeholder="Seu Endereço de E-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha antiga"
+        />
+
+        <Input type="password" name="password" placeholder="Nova senha" />
+
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação da nova senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
   );
 }
